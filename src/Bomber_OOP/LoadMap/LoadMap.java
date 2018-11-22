4c698a617ff43150a8378d2be98295be65b307f1
Nguyenhuy2801 <Nguyenhuy2899@gmail.com>
2018-11-22 17:07:35 +0700

    hihi

--- src/Bomber_OOP/LoadMap/LoadMap.java
+++ src/Bomber_OOP/LoadMap/LoadMap.java
@@ -1,8 +1,8 @@
-package Bomber_OOP.LoadMap;
-
-public class LoadMap {
-    String pathLevel1 = "src/Map1/Map1.txt";
-    public static void LoadLevel1(String path){
-        File
-    }
-}
+//package Bomber_OOP.LoadMap;
+//
+//public class LoadMap {
+//    String pathLevel1 = "src/Map1/Map1.txt";
+//    public static void LoadLevel1(String path){
+//        File
+//    }
+//}
